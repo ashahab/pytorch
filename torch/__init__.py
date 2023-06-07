ashahab/pytorch

commit 07e66ba4172fe84903a79913a0d46d1e260d8ec5

Back out "Remove `check` from `_prims_common`, replace with `torch._check*` (#102219)", Back out "Forwatd fix for D46427687" (#103128)
Test Plan: revertitparrot

Reviewed By: malfet

Differential Revision: D46506433

Pull Request resolved: https://github.com/pytorch/pytorch/pull/103128
Approved by: https://github.com/malfet

torch/__init__.py
@@ -936,8 +936,8 @@ def is_warn_always_enabled():
 # These error checking functions must be kept consistent with their C++
 # equivalents. Their C++ equivalents are mentioned where applicable.
 
-def _check_with(error_type, cond: builtins.bool, message: Callable[[], str]):
-    if not isinstance(cond, (builtins.bool, torch.SymBool)):
+def _check_with(error_type, cond, message):
+    if not isinstance(cond, builtins.bool):
         raise TypeError(f'cond must be a bool, but got {type(cond)}')
 
     if cond:

torch/_prims/__init__.py
@@ -16,6 +16,7 @@ from torch._prims.debug_prims import register_debug_prims
 from torch._prims.nvfuser_prims import register_nvprims
 from torch._prims.rng_prims import register_rng_prims
 from torch._prims_common import (
+    check,
     Dim,
     DimsSequenceType,
     DimsType,
@@ -418,7 +419,7 @@ def _elementwise_meta(
 
 
 def _complex_only_elementwise_meta(*args, **kwargs):
-    torch._check(
+    utils.check(
         utils.is_complex_dtype(args[0].dtype), lambda: "Only complex dtype is supported"
     )
     return _elementwise_meta(*args, **kwargs)
@@ -577,7 +578,7 @@ bitwise_not = _make_elementwise_unary_prim(
 
 
 def _cbrt_aten(a: torch.Tensor) -> Tensor:
-    torch._check(
+    utils.check(
         not a.is_complex(),
         lambda: "cbrt: Complex inputs not supported. Consider calling torch.pow(a, 1.0/3.0)",
     )
@@ -1289,9 +1290,10 @@ def _validate_collapse_args(a: Tensor, start: int, end: int) -> None:
 
     # Verifies end is strictly greater than start
     # (Collapse requires a non-empty interval)
-    torch._check_value(
+    utils.check(
         end >= start,
         lambda: f"Attempting to collapse but end, {end}, is less than start, {start}!",
+        ValueError,
     )
 
 
@@ -1818,7 +1820,7 @@ def _as_strided_scatter_meta(
     utils.validate_strides(stride)
 
     required_size = utils.compute_required_storage_length(size, stride, storage_offset)
-    torch._check(
+    utils.check(
         input.numel() >= required_size,
         lambda: (
             f"as_strided_scatter: sizes {size}, strides {stride}, storage offset {storage_offset} "
@@ -1827,7 +1829,7 @@ def _as_strided_scatter_meta(
             f"for storage of size {input.numel() * input.element_size()}"
         ),
     )
-    torch._check(
+    utils.check(
         utils.is_same_shape(src.shape, size),
         lambda: f"expected src to have a size equal to the slice of self. src size = {src.shape}, slice size = {size}",
     )
@@ -2427,11 +2429,11 @@ def _iota_meta(
     device: torch.device,
     requires_grad: bool,
 ) -> TensorLikeType:
-    torch._check(
+    utils.check(
         utils.is_integer_dtype(dtype),
         lambda: "prims.iota only supports integer dtypes",
     )
-    torch._check(step != 0, lambda: "step must be nonzero")
+    utils.check(step != 0, lambda: "step must be nonzero")
     return torch.empty(
         length,
         dtype=dtype,
@@ -2527,7 +2529,7 @@ def _empty_permuted_meta(
 ) -> TensorLikeType:
     p_strides = utils.make_contiguous_strides_for([shape[l] for l in physical_layout])
     dim = len(shape)
-    torch._check(
+    utils.check(
         len(physical_layout) == dim,
         lambda: (
             "Number of dimensions in the tensor input does not match the "
@@ -2538,7 +2540,7 @@ def _empty_permuted_meta(
     strides = [0] * len(shape)
     seen_dims = set()
     for p, l in enumerate(physical_layout):
-        torch._check(
+        utils.check(
             0 <= l < dim,
             lambda: (
                 f"Dimension out of range (expected to be between 0 and {dim - 1}, but got "
@@ -2546,7 +2548,7 @@ def _empty_permuted_meta(
                 "not currently supported; file an issue if you want it."
             ),
         )
-        torch._check(l not in seen_dims, lambda: "Duplicate dim not allowed")
+        utils.check(l not in seen_dims, lambda: "Duplicate dim not allowed")
         strides[l] = p_strides[p]
         seen_dims.add(l)
     return TensorMeta(
@@ -2774,12 +2776,12 @@ def _normal_meta(
     device: torch.device,
     requires_grad: bool,
 ) -> TensorLikeType:
-    torch._check(
+    utils.check(
         std >= 0.0,
         lambda: f"expected non-negative standard deviation, but got std={std}",
     )
 
-    torch._check(
+    utils.check(
         utils.is_float_dtype(dtype) or utils.is_complex_dtype(dtype),
         lambda: f"expected a floating-point or complex dtype, but got dtype={dtype}",
     )

torch/_prims_common/__init__.py
@@ -268,7 +268,7 @@ _memory_formats = {
 
 
 def validate_memory_format(memory_format: torch.memory_format):
-    torch._check(
+    check(
         memory_format in _memory_formats,
         lambda: f"Received unknown memory format {memory_format}!",
     )
@@ -286,7 +286,7 @@ def is_contiguous_for_memory_format(  # type: ignore[return]
     if memory_format == torch.channels_last_3d:
         return is_channels_last_contiguous_3d(a)
 
-    torch._check(
+    check(
         False,
         lambda: f"is_contiguous received unsupported memory format {memory_format}",
     )
@@ -795,13 +795,13 @@ def infer_size(shape: ShapeType, numel: int) -> Tuple[int, ...]:
     newsize = 1
     for i, d in enumerate(shape):
         if d == -1:
-            torch._check(dim is None, lambda: "only one dimension can be inferred")
+            check(dim is None, lambda: "only one dimension can be inferred")
             dim = i
         elif d >= 0:
             newsize *= d
         else:
-            torch._check(False, lambda: f"invalid shape dimension {d}")
-    torch._check(
+            check(False, lambda: f"invalid shape dimension {d}")
+    check(
         numel == newsize or (dim is not None and newsize > 0 and numel % newsize == 0),
         lambda: f"shape '{list(shape)}' is invalid for input of size {numel}",
     )
@@ -809,7 +809,7 @@ def infer_size(shape: ShapeType, numel: int) -> Tuple[int, ...]:
         # Convert to list to produce a compatible error message with core
         # PyTorch, which prints sequences in square brackets.
         shape = list(shape)
-        torch._check(
+        check(
             newsize != 0,
             lambda: (f"cannot reshape tensor of 0 elements into shape {shape} because the "
                      f"unspecified dimension size -1 can be any value and is ambiguous"),
@@ -954,18 +954,18 @@ def check_fp_or_complex(
     Checks whether the input is floating point or complex.
     If allow_low_precision_dtypes is True, it allows having float16, bfloat16, and complex32
     """
-    torch._check(
+    check(
         is_float_dtype(dtype) or is_complex_dtype(dtype),
         lambda: f"{fn_name}: Expected a floating point or complex tensor as input. Got {dtype}",
     )
-    torch._check(
+    check(
         allow_low_precision_dtypes or not is_low_precision_dtype(dtype),
         lambda: f"{fn_name}: Half precision dtypes not supported. Got {dtype}",
     )
 
 
 def check_is_matrix(A: TensorLikeType, f_name: str, arg_name: str = "A"):
-    torch._check(
+    check(
         len(A.shape) >= 2,
         lambda: f"{f_name}: The input tensor {arg_name} must have at least 2 dimensions.",
     )
@@ -1060,11 +1060,11 @@ def get_higher_dtype(
 
 
 def check_pin_memory(pin_memory: bool):
-    torch._check_not_implemented(not pin_memory, lambda: "PrimTorch does not support pinned memory")
+    check(not pin_memory, lambda: "PrimTorch does not support pinned memory", NotImplementedError)
 
 
 def check_layout(layout: torch.layout):
-    torch._check_not_implemented(layout == torch.strided, lambda: f"PrimTorch doesn't support layout={layout}")
+    check(layout == torch.strided, lambda: f"PrimTorch doesn't support layout={layout}", NotImplementedError)
 
 
 # TODO: maybe unify with can_cast_to?
@@ -1485,7 +1485,7 @@ def make_contiguous_strides_for(
 
 
 def make_channels_last_1d_strides_for(shape: ShapeType) -> Tuple[int, ...]:
-    torch._check(
+    check(
         len(shape) == 3,
         lambda: "Only tensors of rank 3 can use the channels_last_1d memory format",
     )
@@ -1503,7 +1503,7 @@ def make_channels_last_1d_strides_for(shape: ShapeType) -> Tuple[int, ...]:
 
 def make_channels_last_2d_strides_for(shape: ShapeType) -> Tuple[int, ...]:
     # TODO: maybe inform the user of channels_last_3d if rank of the tensor is 5?
-    torch._check(
+    check(
         len(shape) == 4,
         lambda: "Only tensors of rank 4 can use the channels_last memory format",
     )
@@ -1520,7 +1520,7 @@ def make_channels_last_2d_strides_for(shape: ShapeType) -> Tuple[int, ...]:
 
 
 def make_channels_last_3d_strides_for(shape: ShapeType) -> Tuple[int, ...]:
-    torch._check(
+    check(
         len(shape) == 5,
         lambda: "Only tensors of rank 5 can use the channels_last_3d memory format",
     )
@@ -1652,6 +1652,19 @@ def check_in_bounds_for_storage(
             )
         )
         raise ValueError(msg)
+
+
+def check(
+    b: bool, s: Callable[[], str], exc_type: Type[Exception] = RuntimeError
+) -> None:
+    """
+    Helper function for raising an error_type (default: RuntimeError) if a boolean condition fails.
+    Error message is a callable producing a string (to avoid wasting time
+    string formatting in non-error case, and also to make it easier for torchdynamo
+    to trace.)
+    """
+    if not b:
+        raise exc_type(s())
 
 
 # This combines is_channels_last_strides_2d and is_channels_last_strides_3d in

torch/_refs/linalg/__init__.py
@@ -11,6 +11,7 @@ import torch._refs as refs
 import torch._refs.linalg as linalg
 from torch import Tensor
 from torch._prims_common import (
+    check,
     check_fp_or_complex,
     check_is_matrix,
     Dim,
@@ -34,11 +35,11 @@ def check_norm_dtype(dtype: Optional[torch.dtype], x_dtype: torch.dtype, fn_name
     Checks related to the dtype kwarg in `linalg.*norm` functions
     """
     if dtype is not None:
-        torch._check(
+        check(
             utils.is_float_dtype(dtype) or utils.is_complex_dtype(dtype),
             lambda: f"{fn_name}: dtype should be floating point or complex. Got {dtype}",
         )
-        torch._check(
+        check(
             utils.is_complex_dtype(dtype) == utils.is_complex_dtype(x_dtype),
             lambda: "{fn_name}: dtype should be {d} for {d} inputs. Got {dtype}".format(
                 fn_name=fn_name,
@@ -46,7 +47,7 @@ def check_norm_dtype(dtype: Optional[torch.dtype], x_dtype: torch.dtype, fn_name
                 dtype=dtype,
             ),
         )
-        torch._check(
+        check(
             utils.get_higher_dtype(dtype, x_dtype) == dtype,
             lambda: f"{fn_name}: the dtype of the input ({x_dtype}) should be convertible "
             "without narrowing to the specified dtype ({dtype})",
@@ -84,7 +85,7 @@ def vector_norm(
         dim = [dim]  # type: ignore[assignment]
 
     if x.numel() == 0 and (ord < 0.0 or ord == float("inf")):
-        torch._check(
+        check(
             dim is not None and len(dim) != 0,
             lambda: f"linalg.vector_norm cannot compute the {ord} norm on an empty tensor "
             "because the operation does not have an identity",
@@ -92,7 +93,7 @@ def vector_norm(
         shape = x.shape
         assert dim is not None  # mypy does not seem to be able to see through check?
         for d in dim:
-            torch._check(
+            check(
                 shape[d] != 0,
                 lambda: f"linalg.vector_norm cannot compute the {ord} norm on the "
                 f"dimension {d} because this dimension is empty and the "
@@ -152,10 +153,8 @@ def matrix_norm(
     dim = utils.canonicalize_dims(A.ndim, dim)
     if isinstance(dim, Dim):
         dim = (dim,)  # type: ignore[assignment]
-    torch._check(
-        len(dim) == 2, lambda: "linalg.matrix_norm: dim must be a 2-tuple. Got {dim}"
-    )
-    torch._check(
+    check(len(dim) == 2, lambda: "linalg.matrix_norm: dim must be a 2-tuple. Got {dim}")
+    check(
         dim[0] != dim[1],
         lambda: "linalg.matrix_norm: dims must be different. Got ({dim[0]}, {dim[1]})",
     )
@@ -164,7 +163,7 @@ def matrix_norm(
 
     if isinstance(ord, str):
         # ord
-        torch._check(
+        check(
             ord in ("fro", "nuc"),
             lambda: "linalg.matrix_norm: Order {ord} not supported.",
         )
@@ -187,7 +186,7 @@ def matrix_norm(
     else:
         # ord
         abs_ord = abs(ord)
-        torch._check(
+        check(
             abs_ord in (2, 1, float("inf")),
             lambda: "linalg.matrix_norm: Order {ord} not supported.",
         )
@@ -231,12 +230,12 @@ def norm(
     if dim is not None:
         if isinstance(dim, Dim):
             dim = (dim,)  # type: ignore[assignment]
-        torch._check(
+        check(
             len(dim) in (1, 2),
             lambda: "linalg.norm: If dim is specified, it must be of length 1 or 2. Got {dim}",
         )
     elif ord is not None:
-        torch._check(
+        check(
             A.ndim in (1, 2),
             lambda: "linalg.norm: If dim is not specified but ord is, the input must be 1D or 2D. Got {A.ndim}D",
         )

torch/_refs/nn/functional/__init__.py
@@ -8,6 +8,7 @@ import torch._prims_common as utils
 import torch._refs as refs
 from torch._decomp import register_decomposition
 from torch._prims_common import (
+    check,
     ELEMENTWISE_TYPE_PROMOTION_KIND,
     NumberType,
     ShapeType,
@@ -88,7 +89,7 @@ def alpha_dropout(
     if not training:
         return self
 
-    torch._check(
+    utils.check(
         p <= 1 and p >= 0,
         lambda: f"dropout probability has to be between 0 and 1, but got, {p}",
     )
@@ -124,7 +125,7 @@ def inplace_wrapper(fn):
     @wraps(fn)
     def _fn(a, *args, inplace=False, **kwargs):
         if inplace:
-            torch._check(
+            check(
                 "out" not in kwargs,
                 lambda: "Cannot set inplace=True and pass out= at the same time",
             )
@@ -183,7 +184,7 @@ def dropout(
     if not training:
         return a
 
-    torch._check(
+    utils.check(
         p <= 1 and p >= 0,
         lambda: f"dropout probability has to be between 0 and 1, but got, {p}",
     )
@@ -222,15 +223,15 @@ def elu(
 
     # nb. This should be factored out into a can_cast aux function
     python_type = utils.dtype_to_type(a.dtype)
-    torch._check(
+    check(
         utils.is_weakly_lesser_type(type(input_scale), python_type),
         lambda: f"input_scale argument of type {type(input_scale)} cannot be safely cast to type {python_type}!",
     )
-    torch._check(
+    check(
         utils.is_weakly_lesser_type(type(scale), python_type),
         lambda: f"scale argument of type {type(scale)} cannot be safely cast to type {python_type}!",
     )
-    torch._check(
+    check(
         utils.is_weakly_lesser_type(type(alpha), python_type),
         lambda: f"alpha argument of type {type(alpha)} cannot be safely cast to type {python_type}!",
     )
@@ -266,14 +267,14 @@ def group_norm(
     """
     Reference implementation of :func:`torch.nn.functional.group_norm`.
     """
-    torch._check(
+    utils.check(
         input.ndim >= 2,
         lambda: f"Expected at least 2 dimensions for input tensor but received {input.ndim}",
     )
 
     batch_size = input.shape[0]
     num_channels = input.shape[1]
-    torch._check(
+    utils.check(
         num_channels % num_groups == 0,
         lambda: "Expected number of channels in input to be divisible by num_groups, "
         + f"but got input of shape {input.shape} and num_groups = {num_groups}",
@@ -384,7 +385,7 @@ def softmax(
     # deprecated.  For PrimTorch, it's fine to drop support for deprecated
     # behavior because it requires explicit opt in.  This error is to inform
     # users how to update their calls.
-    torch._check(dim is not None, lambda: "implicit dim not supported, use dim=X")
+    check(dim is not None, lambda: "implicit dim not supported, use dim=X")
     return torch.softmax(a=a, dim=dim, dtype=dtype)  # type: ignore[call-overload]
 
 
@@ -399,7 +400,7 @@ def softmin(
     # deprecated.  For PrimTorch, it's fine to drop support for deprecated
     # behavior because it requires explicit opt in.  This error is to inform
     # users how to update their calls.
-    torch._check(dim is not None, lambda: "implicit dim not supported, use dim=X")
+    check(dim is not None, lambda: "implicit dim not supported, use dim=X")
     return torch.softmax(a=-a, dim=dim, dtype=dtype)  # type: ignore[call-overload]
 
 
@@ -459,7 +460,7 @@ def softshrink(a: TensorLikeType, lambd: float = 0.5):
     # softshrink(x) = x - lambd if x > lambd
     #               = x + lambd if x < -lambd
     #               = 0 otherwise
-    torch._check(
+    check(
         lambd >= 0,
         lambda: f"lambda must be greater or equal to 0, but found to be {lambd}",
     )
@@ -586,7 +587,7 @@ def log_softmax(
     # deprecated.  For PrimTorch, it's fine to drop support for deprecated
     # behavior because it requires explicit opt in.  This error is to inform
     # users how to update their calls.
-    torch._check(dim is not None, lambda: "implicit dim not supported, use dim=X")
+    check(dim is not None, lambda: "implicit dim not supported, use dim=X")
     return torch.log_softmax(a=a, dim=dim, dtype=dtype)  # type: ignore[call-overload]
 
 
@@ -658,12 +659,12 @@ def _nll_loss_nd(
     reduction: str,
     ignore_index: int,
 ) -> TensorLikeType:
-    torch._check(
+    utils.check(
         input.ndim > 0 and input.ndim <= 3,
         lambda: f"Expected input dimension to be either [1, 2, 3] but received {input.ndim}.",
     )
 
-    torch._check(
+    utils.check(
         (input.ndim == 1) or (input.shape[0] == target.shape[0]),
         lambda: f"Expected input batch size {input.shape[0]} to match target batch size {target.shape[0]}.",
     )
@@ -683,7 +684,7 @@ def _nll_loss_nd(
         (flat_target >= 0), (flat_target < num_classes)
     )
     class_check = torch.all(torch.logical_or(ignore_classes_mask, valid_classes_mask))
-    torch._check(
+    utils.check(
         isinstance(target, FakeTensor) or bool(class_check.item()),
         lambda: "A target class is out-of-bounds and not the ignore index.",
     )
@@ -748,7 +749,7 @@ def nll_loss(
     """
     Reference implementation of torch.nn.functional.nll_loss
     """
-    torch._check(
+    utils.check(
         input.ndim > 0,
         lambda: f"Expected input tensor to have 1 or more dimensions (got {input.ndim})",
     )
@@ -786,13 +787,9 @@ def nll_loss(
     # For ndim > 3, we reshape the input and target to 3-D case.
     # Input (N batch-size, C classes, k-dimensions)
     # Target (N batch-size, k-dimensions)
-    torch._check(
+    utils.check(
         input.ndim > 0 and target.ndim > 0 and target.shape[1:] == input.shape[2:],
-        lambda: (
-            "Expected input and target to both have ndim > 0 and "
-            "target.shape[1:] == input.shape[2:], but got "
-            f"target.shape {target.shape} and input.shape {input.shape}"
-        ),
+        lambda: f"Expected target shape {out_size} but got {target.shape}",
     )
 
     batch_size = input.shape[0]
@@ -831,7 +828,7 @@ def huber_loss(
     if type(reduction) is int:
         reduction = _reduction_int_to_str(reduction)
     _check_reduction_value(reduction)  # type: ignore[arg-type]
-    torch._check(
+    check(
         delta > 0,
         lambda: "huber_loss does not support non-positive values for delta.",
     )
@@ -932,7 +929,7 @@ def _triplet_margin_with_distance_loss(
     a_dim = anchor.ndim
     p_dim = positive.ndim
     n_dim = negative.ndim
-    torch._check(
+    check(
         a_dim == p_dim and p_dim == n_dim,
         lambda: (
             f"The anchor, positive, and negative tensors are expected to have "
@@ -1069,25 +1066,25 @@ def prelu(a: TensorLikeType, weight: TensorLikeType) -> TensorLikeType:
     """
     Reference implementation of torch.nn.functional.prelu
     """
-    torch._check(
+    check(
         isinstance(a, TensorLike),
         lambda: f"prelu: Expected `a` to be tensor, but got: {type(a)}",
     )
-    torch._check(
+    check(
         isinstance(weight, TensorLike),
         lambda: f"prelu: Expected `weight` to be tensor, but got: {type(weight)}",
     )
 
     if weight.numel() != 1:
-        torch._check(a.ndim > 0, lambda: "Not allow zero-dim input tensor.")
+        check(a.ndim > 0, lambda: "Not allow zero-dim input tensor.")
         channel_size = a.shape[1] if a.ndim >= 2 else 1
-        torch._check(
+        check(
             weight.numel() == channel_size,
             lambda: f"Mismatch of parameter numbers and input channel size. Found parameter numbers ="
             f" {weight.numel()} and channel size = {channel_size}.",
         )
 
-    torch._check(
+    check(
         weight.ndim == 0 or weight.ndim == 1,
         lambda: f"prelu: Expected `weight` to be a scalar or 1D tensor, but got: "
         f"ndim = {weight.ndim}",
@@ -1126,7 +1123,7 @@ def relu6(a: TensorLikeType, inplace: bool = False) -> TensorLikeType:
 )
 def glu(a: TensorLikeType, dim: int = -1) -> TensorLikeType:
     dim = utils.canonicalize_dims(a.ndim, dim)
-    torch._check(
+    check(
         a.shape[dim] % 2 == 0,
         lambda: f"Halving dimension must be even, but dimension {dim} is size {a.shape[dim]}",
     )
@@ -1154,8 +1151,8 @@ def pairwise_distance(
     type_promotion_kind=utils.ELEMENTWISE_TYPE_PROMOTION_KIND.DEFAULT,
 )
 def pdist(a: TensorLikeType, p: float = 2) -> TensorLikeType:
-    torch._check(a.ndim == 2, lambda: f"pdist only supports 2D tensors, got: {a.ndim}D")
-    torch._check(p >= 0, lambda: "pdist only supports non-negative p values")
+    check(a.ndim == 2, lambda: f"pdist only supports 2D tensors, got: {a.ndim}D")
+    check(p >= 0, lambda: "pdist only supports non-negative p values")
     # For p == 2 we can use an efficient implementation, but other values of p
     # require creating a much bigger tensor for an intermediate step
     if p == 2:

torch/_refs/special/__init__.py
@@ -147,7 +147,7 @@ def logit(self: TensorLikeType, eps: Optional[float] = None) -> TensorLikeType:
     type_promotion_kind=ELEMENTWISE_TYPE_PROMOTION_KIND.INT_TO_FLOAT,
 )
 def xlog1py(a: Union[TensorLikeType, NumberType], b: Union[TensorLikeType, NumberType]):
-    torch._check(
+    utils.check(
         isinstance(a, TensorLike) or isinstance(b, TensorLike),
         lambda: 'Expected either argument a or b to be a Tensor"',
     )

commit 178286a2ab85370c586556970c3d81fe565db241

Fix regressions caused by https://github.com/pytorch/pytorch/pull/103128
By adding `torch.SymBool` back

torch/__init__.py
@@ -937,7 +937,7 @@ def is_warn_always_enabled():
 # equivalents. Their C++ equivalents are mentioned where applicable.
 
 def _check_with(error_type, cond, message):
-    if not isinstance(cond, builtins.bool):
+    if not isinstance(cond, (builtins.bool, torch.SymBool)):
         raise TypeError(f'cond must be a bool, but got {type(cond)}')
 
     if cond: