ashahab/pytorch

commit 7b5f8988a24efc14897ef47d9f44ede43250ad92

[GHF] Auth when trying to fetch labels (#103679)
There were few merge bot failures reported recently due to HTTP/403 error:
- https://github.com/pytorch/pytorch/actions/runs/5269083146/jobs/9526693976#step:6:80
- https://github.com/pytorch/pytorch/actions/runs/5272750075/jobs/9535376256#step:6:93

Which likely stems from the fact that `_fetch_url` method did not try to pass the auth token even when it was available and as result was rate-limited to 60 requests per hour, according to [Resources in the REST API](https://docs.github.com/en/rest/overview/resources-in-the-rest-api?apiVersion=2022-11-28#rate-limit-headers)

Refactor `gh_fetch_url` into `gh_fetch_url_and_headers` and use it from `request_for_labels` to utilize auth token, if available, which bumps rate limit to 1000 per hour as well as print more actionable message when rate limit is exceeded.

<!--
copilot:poem
-->
### <samp>🤖 Generated by Copilot at 499b805</samp>

> _`gh_fetch_url` splits_
> _returns headers and body_
> _wrapper function_

Pull Request resolved: https://github.com/pytorch/pytorch/pull/103679
Approved by: https://github.com/jeanschmidt, https://github.com/kit1980

.github/scripts/github_utils.py
@@ -4,7 +4,7 @@ import json
 import os
 
 from dataclasses import dataclass
-from typing import Any, Callable, cast, Dict, List, Optional
+from typing import Any, Callable, cast, Dict, List, Optional, Tuple
 from urllib.error import HTTPError
 from urllib.parse import quote
 from urllib.request import Request, urlopen
@@ -21,14 +21,14 @@ class GitHubComment:
     url: str
 
 
-def gh_fetch_url(
+def gh_fetch_url_and_headers(
     url: str,
     *,
     headers: Optional[Dict[str, str]] = None,
     data: Optional[Dict[str, Any]] = None,
     method: Optional[str] = None,
     reader: Callable[[Any], Any] = lambda x: x.read(),
-) -> Any:
+) -> Tuple[Any, Any]:
     if headers is None:
         headers = {}
     token = os.environ.get("GITHUB_TOKEN")
@@ -37,7 +37,7 @@ def gh_fetch_url(
     data_ = json.dumps(data).encode() if data is not None else None
     try:
         with urlopen(Request(url, headers=headers, data=data_, method=method)) as conn:
-            return reader(conn)
+            return conn.headers, reader(conn)
     except HTTPError as err:
         if err.code == 403 and all(
             key in err.headers for key in ["X-RateLimit-Limit", "X-RateLimit-Used"]
@@ -50,6 +50,19 @@ def gh_fetch_url(
                 Resets at: {err.headers['x-RateLimit-Reset']}"""
             )
         raise
+
+
+def gh_fetch_url(
+    url: str,
+    *,
+    headers: Optional[Dict[str, str]] = None,
+    data: Optional[Dict[str, Any]] = None,
+    method: Optional[str] = None,
+    reader: Callable[[Any], Any] = lambda x: x.read(),
+) -> Any:
+    return gh_fetch_url_and_headers(
+        url, headers=headers, data=data, reader=json.load, method=method
+    )[0]
 
 
 def gh_fetch_json(

.github/scripts/label_utils.py
@@ -4,9 +4,8 @@ import json
 
 from functools import lru_cache
 from typing import Any, List, Tuple, TYPE_CHECKING, Union
-from urllib.request import Request, urlopen
 
-from github_utils import gh_fetch_url, GitHubComment
+from github_utils import gh_fetch_url_and_headers, GitHubComment
 
 # TODO: this is a temp workaround to avoid circular dependencies,
 #       and should be removed once GitHubPR is refactored out of trymerge script.
@@ -29,15 +28,11 @@ https://github.com/pytorch/pytorch/wiki/PyTorch-AutoLabel-Bot#why-categorize-for
 """
 
 
-# Modified from https://github.com/pytorch/pytorch/blob/b00206d4737d1f1e7a442c9f8a1cadccd272a386/torch/hub.py#L129
-def _read_url(url: Request) -> Tuple[Any, Any]:
-    with urlopen(url) as r:
-        return r.headers, r.read().decode(r.headers.get_content_charset("utf-8"))
-
-
 def request_for_labels(url: str) -> Tuple[Any, Any]:
     headers = {"Accept": "application/vnd.github.v3+json"}
-    return _read_url(Request(url, headers=headers))
+    return gh_fetch_url_and_headers(
+        url, headers=headers, reader=lambda x: x.read().decode("utf-8")
+    )
 
 
 def update_labels(labels: List[str], info: str) -> None:
@@ -77,14 +72,14 @@ def gh_get_labels(org: str, repo: str) -> List[str]:
 def gh_add_labels(
     org: str, repo: str, pr_num: int, labels: Union[str, List[str]]
 ) -> None:
-    gh_fetch_url(
+    gh_fetch_url_and_headers(
         url=f"https://api.github.com/repos/{org}/{repo}/issues/{pr_num}/labels",
         data={"labels": labels},
     )
 
 
 def gh_remove_label(org: str, repo: str, pr_num: int, label: str) -> None:
-    gh_fetch_url(
+    gh_fetch_url_and_headers(
         url=f"https://api.github.com/repos/{org}/{repo}/issues/{pr_num}/labels/{label}",
         method="DELETE",
     )

commit 192351b91093f3c768a4b24dc47303046341d595

[GHF] Fix regression
Introduced by https://github.com/pytorch/pytorch/pull/103679

That was not covered by tests :(

Discovered while looking at https://github.com/pytorch/pytorch/actions/runs/5281833681/jobs/9555936751#step:5:24

Locally tested by running `python -c "from trymerge import gh_get_pr_info;print(gh_get_pr_info('pytorch', 'pytorch', 103685)['author'])"`

.github/scripts/github_utils.py
@@ -62,7 +62,7 @@ def gh_fetch_url(
 ) -> Any:
     return gh_fetch_url_and_headers(
         url, headers=headers, data=data, reader=json.load, method=method
-    )[0]
+    )[1]
 
 
 def gh_fetch_json(