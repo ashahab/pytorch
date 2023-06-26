ashahab/pytorch

commit d1c72325c052aa13fbb9942c83f4d716a5894ab6

[Pytorch] aten::expand (#103930)
Summary:
Pull Request resolved: https://github.com/pytorch/pytorch/pull/103930

Expand using `aten::repeat` for all dims

[expand](https://pytorch.org/docs/stable/generated/torch.Tensor.expand.html#torch.Tensor.expand)
[expand_as](
https://pytorch.org/docs/stable/generated/torch.Tensor.expand_as.html)

Test Plan:
clang-format on `Expand.cpp`
expand tests:
```
lfq@lfq-mbp fbsource % buck run --target-platforms ovr_config//platform/macos:arm64-fbsource //xplat/caffe2:pt_vulkan_api_test_binAppleMac\#macosx-arm64 -c pt.vulkan_full_precision=1 -- --gtest_filter="*.expand*"
Action graph will be rebuilt because files have been added or removed.

Parsing buck files: finished in 1.1 sec
Downloaded 5/50 artifacts, 661.18 Kbytes, 37.5% cache miss (for updated rules)
Building: finished in 15.4 sec (100%) 515/515 jobs, 15/515 updated
  Total time: 16.9 sec
BUILD SUCCEEDED
Running main() from xplat/third-party/gmock/googletest-1.12.1/googletest/src/gtest_main.cc
Note: Google Test filter = *.expand*
[==========] Running 6 tests from 1 test suite.
[----------] Global test environment set-up.
[----------] 6 tests from VulkanAPITest
[ RUN      ] VulkanAPITest.expand_exceptions
[       OK ] VulkanAPITest.expand_exceptions (66 ms)
[ RUN      ] VulkanAPITest.expand_1d
[       OK ] VulkanAPITest.expand_1d (7 ms)
[ RUN      ] VulkanAPITest.expand_2d
[       OK ] VulkanAPITest.expand_2d (2 ms)
[ RUN      ] VulkanAPITest.expand_3d
[       OK ] VulkanAPITest.expand_3d (2 ms)
[ RUN      ] VulkanAPITest.expand_4d
[       OK ] VulkanAPITest.expand_4d (4 ms)
[ RUN      ] VulkanAPITest.expand_as
[       OK ] VulkanAPITest.expand_as (11 ms)
[----------] 6 tests from VulkanAPITest (95 ms total)

[----------] Global test environment tear-down
[==========] 6 tests from 1 test suite ran. (95 ms total)
[  PASSED  ] 6 tests.
lfq@lfq-mbp fbsource %
```

Reviewed By: SS-JIA

Differential Revision: D46302042

fbshipit-source-id: e75dd81b399fc519de49852a7e2713e3c179a9c0

aten/src/ATen/native/vulkan/ops/Expand.cpp
@@ -1,0 +1,87 @@
+#include <ATen/native/vulkan/ops/Common.h>
+#include <ATen/native/vulkan/ops/Utils.h>
+#include <torch/library.h>
+
+#ifndef AT_PER_OPERATOR_HEADERS
+#include <ATen/Functions.h>
+#else
+#include <ATen/ops/repeat.h>
+#endif
+
+#include <ATen/native/vulkan/ops/Utils.h>
+
+namespace at {
+namespace native {
+namespace vulkan {
+namespace ops {
+namespace {
+
+using namespace api::utils;
+
+Tensor expand(
+    const at::Tensor& self,
+    const IntArrayRef output_size,
+    bool implicit = false) {
+  TORCH_CHECK(
+      self.dim() > 0 && self.dim() <= 4,
+      "Vulkan expand supports up to 4d tensors");
+  TORCH_CHECK(
+      self.dim() <= output_size.size(),
+      "Vulkan expand: the number of sizes provided (",
+      output_size.size(),
+      ") must be greater or equal to the number of dimensions in the tensor (",
+      self.dim(),
+      ").");
+
+  std::vector<int64_t> repeat_size = std::vector<int64_t>(output_size.size());
+  std::vector<int64_t> input_size = self.sizes().vec();
+
+  int in_idx = input_size.size() - 1;
+  for (int i = output_size.size() - 1; i >= 0; --i) {
+    if (in_idx >= 0) {
+      TORCH_CHECK(
+          input_size[in_idx] == output_size[i] || input_size[in_idx] == 1 ||
+              output_size[i] == -1,
+          "Vulkan expand: the expanded size of the tensor (",
+          output_size[i],
+          ") must match the existing size (",
+          input_size[in_idx],
+          ") at non-singleton dimension ",
+          i);
+
+      if (input_size[in_idx] == output_size[i] || output_size[i] == -1) {
+        repeat_size[i] = 1;
+      } else if (input_size[in_idx] == 1) {
+        repeat_size[i] = output_size[i];
+      }
+      --in_idx;
+    } else {
+      TORCH_CHECK(
+          output_size[i] != -1,
+          "Vulkan expand: the expanded size of the tensor (-1) is not allowed in a leading, non-existing dimension 0.");
+
+      repeat_size[i] = output_size[i];
+    }
+  }
+
+  return self.repeat(repeat_size);
+}
+
+Tensor expand_as(const at::Tensor& self, const at::Tensor& other) {
+  return expand(self, other.sizes());
+}
+
+#ifdef USE_VULKAN_API
+
+TORCH_LIBRARY_IMPL(aten, Vulkan, m) {
+  m.impl(TORCH_SELECTIVE_NAME("aten::expand"), TORCH_FN(expand));
+  m.impl(TORCH_SELECTIVE_NAME("aten::expand_as"), TORCH_FN(expand_as));
+}
+
+#endif /* USE_VULKAN_API */
+
+} // namespace
+} // namespace ops
+} // namespace vulkan
+} // namespace native
+} // namespace at

aten/src/ATen/test/vulkan_api_test.cpp
@@ -1816,6 +1816,91 @@ TEST_F(VulkanAPITest, empty) {
   ASSERT_NO_THROW(at::empty({1, 17, 41, 53}, at::device(at::kVulkan).dtype(at::kFloat)));
 }
 
+void test_expand(const at::IntArrayRef input_shape, const at::IntArrayRef output_shape) {
+  c10::InferenceMode mode;
+  const auto cpu = at::rand(input_shape, at::device(at::kCPU).dtype(at::kFloat));
+  const auto vulkan = cpu.vulkan();
+
+  cpu.expand(output_shape);
+  vulkan.expand(output_shape);
+
+  const auto check = almostEqual(cpu, vulkan.cpu());
+  if (!check) {
+    showRtol(cpu, vulkan.cpu());
+  }
+  ASSERT_TRUE(check);
+}
+
+TEST_F(VulkanAPITest, expand_exceptions) {
+  // Vulkan expand supports input dims <= 4
+  auto in_cpu = at::rand({1, 2, 3, 4, 5}, at::device(at::kCPU).dtype(at::kFloat));
+  EXPECT_THROW(const auto out_vulkan = in_cpu.vulkan().expand({1, 2, 3, 4}), ::c10::Error);
+
+  // Vulkan expand supports output_size <= 4
+  in_cpu = at::rand({1, 2, 3, 4}, at::device(at::kCPU).dtype(at::kFloat));
+  EXPECT_THROW(const auto out_vulkan = in_cpu.vulkan().expand({1, 1, 2, 3, 4}), ::c10::Error);
+
+  // Vulkan expand expects output size >= input
+  in_cpu = at::rand({1, 2, 3}, at::device(at::kCPU).dtype(at::kFloat));
+  EXPECT_THROW(const auto out_vulkan = in_cpu.vulkan().expand({2, 3}), ::c10::Error);
+
+  // Non-singleton dimensions must match
+  in_cpu = at::rand({3, 1}, at::device(at::kCPU).dtype(at::kFloat));
+  EXPECT_THROW(const auto out_vulkan = in_cpu.vulkan().expand({1, 1}), ::c10::Error);
+
+  // -1 not allowed in leading, non-existing dimension
+  in_cpu = at::rand({3, 1}, at::device(at::kCPU).dtype(at::kFloat));
+  EXPECT_THROW(const auto out_vulkan = in_cpu.vulkan().expand({-1, 3, 1}), ::c10::Error);
+}
+
+TEST_F(VulkanAPITest, expand_1d) {
+  test_expand({1}, {3});
+
+  test_expand({1}, {9, 3});       // 1d->2d
+  test_expand({1}, {8, 9, 3});    // 1d->3d
+  test_expand({1}, {7, 8, 9, 3}); // 1d->4d
+}
+
+TEST_F(VulkanAPITest, expand_2d) {
+  test_expand({5, 1}, {-1, 5}); // W
+  test_expand({1, 5}, {5, 5});  // H
+
+  test_expand({5, 1}, {2, -1, 5});    // 2d->3d
+  test_expand({1, 5}, {2, 5, 3, -1}); // 2d->4d
+}
+
+TEST_F(VulkanAPITest, expand_3d) {
+  test_expand({3, 4, 1}, {3, 4, -1}); // W
+  test_expand({3, 1, 5}, {-1, 4, 5}); // H
+  test_expand({1, 4, 5}, {3, -1, 5}); // C
+
+  test_expand({5, 4, 3}, {2, -1, -1, -1}); // 3d->4d
+}
+
+TEST_F(VulkanAPITest, expand_4d) {
+  test_expand({5, 4, 3, 1}, {5, 4, 3, 9}); // W
+  test_expand({5, 4, 1, 2}, {5, 4, 9, 2}); // H
+  test_expand({5, 1, 3, 2}, {5, 9, 3, 2}); // C
+  test_expand({1, 4, 3, 2}, {9, 4, 3, 2}); // N
+}
+
+TEST_F(VulkanAPITest, expand_as) {
+  // expand_as calls into expand, without negative sizes, those tests should be sufficient.
+  c10::InferenceMode mode;
+  const auto cpu = at::rand({1, 1, 1, 1}, at::device(at::kCPU).dtype(at::kFloat));
+  const auto vulkan = cpu.vulkan();
+  const auto other = at::rand({9, 11, 33, 22}, at::device(at::kCPU).dtype(at::kFloat));
+
+  cpu.expand_as(other);
+  vulkan.expand_as(other);
+
+  const auto check = almostEqual(cpu, vulkan.cpu());
+  if (!check) {
+    showRtol(cpu, vulkan.cpu());
+  }
+  ASSERT_TRUE(check);
+}
+
 void test_glu(const at::IntArrayRef input_shape) {
   const auto in_cpu = at::rand(input_shape, at::device(at::kCPU).dtype(at::kFloat));
   const auto in_vulkan = in_cpu.vulkan();

commit 2cfe7b6541647b3ed50c9e5bfa92cdcd959a0b48

aten: Ensure dim is size_t (#104201)
Attempts to fix failures introduced in https://github.com/pytorch/pytorch/pull/103930 (example failures: https://github.com/pytorch/pytorch/actions/runs/5363450214/jobs/9731034104)

<!--
copilot:all
-->
### <samp>🤖 Generated by Copilot at 67d5076</samp>

### Summary
🔧🚨🚦

<!--
1.  🔧 (wrench) - This emoji can be used to indicate a bug fix or a minor improvement to the code quality or performance.
2.  🚨 (rotating light) - This emoji can be used to indicate a change that affects the error handling or validation logic of the code, or that adds or modifies a test case.
3.  🚦 (vertical traffic light) - This emoji can be used to indicate a change that affects the control flow or branching logic of the code, or that adds or modifies a condition or assertion.
-->
Fix a compiler warning in `Expand.cpp` by casting a tensor dimension to `size_t`. This improves the code quality and correctness of the `expand` function for the Vulkan backend.

> _`expand` tensor_
> _cast `dim()` to `size_t`_
> _autumn leaves warning_

### Walkthrough
*  Cast `self.dim()` to `size_t` to avoid signed-unsigned comparison warning in `expand` function ([link](https://github.com/pytorch/pytorch/pull/104201/files?diff=unified&w=0#diff-c175e908cbcb8595b22696e672b526202ed3a4a11341603c1522397e499b5c2bL29-R29))

<details>
<summary> Fix done using chatgpt </summary>

![Screenshot 2023-06-26 at 11 52 14 AM](https://github.com/pytorch/pytorch/assets/1700823/95c141e5-36b6-4916-85ca-85415bcc507f)

</details>
Signed-off-by: Eli Uriegas <eliuriegas@meta.com>
Pull Request resolved: https://github.com/pytorch/pytorch/pull/104201
Approved by: https://github.com/lucylq, https://github.com/huydhn, https://github.com/malfet

aten/src/ATen/native/vulkan/ops/Expand.cpp
@@ -26,7 +26,7 @@ Tensor expand(
       self.dim() > 0 && self.dim() <= 4,
       "Vulkan expand supports up to 4d tensors");
   TORCH_CHECK(
-      self.dim() <= output_size.size(),
+      static_cast<size_t>(self.dim()) <= output_size.size(),
       "Vulkan expand: the number of sizes provided (",
       output_size.size(),
       ") must be greater or equal to the number of dimensions in the tensor (",